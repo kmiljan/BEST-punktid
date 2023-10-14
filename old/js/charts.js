c807802ce5222f03f2f4a790fa271aa490ea2366
hannes <hannesharm7@gmail.com>
2023-10-15 00:13:26 +0300

    Some more cleanups

--- old/js/charts.js
+++
@@ -1,7 +0,0 @@
-// Load the Visualization API and the corechart package.
-function loadGChartsAPI() {
-    google.charts.load('current', {'packages':['corechart']});
-    return new Promise((resolve) => {
-        google.charts.setOnLoadCallback(resolve);
-    });
-}
--- old/js/forceCreateHeader.js
+++
@@ -1,8 +0,0 @@
-function navigateToURL(URL) {
-    window.location.href = URL;
-}
-function navigateToHomepage() {
-    navigateToURL('/');
-}
-render.header.create(document.getElementById("header"), navigateToHomepage, undefined);
-render.header.showBackbutton();
