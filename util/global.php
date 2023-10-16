8d8c780ce17a9059944dc4687081a7de2209bab9
hannes <hannesharm7@gmail.com>
2023-10-16 23:23:44 +0300

    Some import fixes

--- util/global.php
+++ util/global.php
@@ -6,10 +6,5 @@ mb_internal_encoding("UTF-8");
 $PUBLIC_PATH = dirname(__DIR__);
 
 require_once($PUBLIC_PATH . "/host/host.php");
-require_once($PUBLIC_PATH . "/util/class.php");
-
-require_once($PUBLIC_PATH . "/config/config.php");
 require_once($PUBLIC_PATH . "/util/time.php");
-require_once($PUBLIC_PATH . "/util/remap.php");
 require_once($PUBLIC_PATH . "/util/utilFunctions.php");
-?>
