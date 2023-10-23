800317b32b9e112ee5d5dfcdebe876a9ac6e1080
hannes <hannesharm7@gmail.com>
2023-10-23 23:04:23 +0300

    Lühike hetkese arhitektuuri kirjeldus

--- pages/architecture.php
+++ pages/architecture.php
@@ -11,8 +11,18 @@
         <div id="screenWrapper">
             <div class="basicScreen" class=" _invisCapable">
                 <h1>Kuidas see kõik töötab?</h1>
-                <h2>Vanasti, kui muru oli rohelisem, ja taevas sinisem, oli see nii:</h2>
-                <h3>Iidsel ajal:</h3>
+                <h2>Modernsemad ajad toovad paremad lahendused:</h2>
+                <p>Nüüdseks on punktisüsteem ja liikmehaldus viidud ühisesse andmebaasi, mistõttu on liikmete punktide pärimine selle võrra ka lihtsam.</p>
+                <h3>Front-end</h3>
+                <p>Veebileht on JavaScript rakendus, mis muuhulgas koostab standardseid tabeleid ja graafikuid, ning paneb need ekraanil ruudustikule.</p>
+                <p>Iga graafik või tabel on jagatud sisemiselt kaheks: <em>display</em> ja <em>render</em> osa. <em>Display</em> on kindla kasutuse, ülesande ning andmeallikaga objekt. Näited on selle kuu parimate tabel või spetsiifiliselt bestika kogupunktide numbri element. Iga <em>display</em> küsib ise andmed ning laseb sobival <em>render</em> objektil teha saadud andmete põhjal graafik, tabel või muu näidik. <em>Render</em> objektid ongi näidikud, tabelid või graafikud, nagu näiteks joongraafik või punkte näitav element.</p>
+                <p>Andmed küsib <em>display</em> element sobiva GET päringuga, millele vastuseks saab genereeritud JSON formaadis andmed.</p>
+                <h3>Back-end</h3>
+                <p>Andmed tabelitesse ja graafikutesse päritakse backend rakendusest, mis omakorda teeb SQL päringud andmebaasi. Hetkel (as of 23.10.2023) ei ole päringute vahele pandud mingisugust cahce süsteemi, et kiirendada samade andmete päringute kiirust ning vähendada andmebaasi koormust.</p>
+                <p>Kõik nähtav statistika arvutatakse backendis ning edastatakse frontendi graafikutele kasutamiseks.</p>
+                <h2>Vanasti, kui muru oli rohelisem ja taevas sinisem, oli see nii:</h2>
+                <p><strong>Alates 21.10.2023 on rakendus viidud üle kasutama uut BEST-Estonia siseveebi andmebaasi.</strong></p>
+                <h3>Punktid sisestati Google Sheetsis</h3>
                 <p>Koordinaatorid täidavad Google Spreadsheets lehti, mis kogutakse iga 5 minuti tagant kokku, salvestatakse BESTi SQL andmebaasi, ning arvutatakse salvestatud andmetelt mõngingat statistikat.</p>
                 <p>Iga kord, kui keegi laeb punktide vaatamiseks lehe, küsib leht backend süsteemilt andmeid, mis omakorda küsib need andmebaasist ning teeb ka ise vajadusel mõned arvutused.</p>
                 <h3>Koordinaatorite tabelid</h3>
@@ -30,13 +40,9 @@
                 <p>Veebileht on JavaScript rakendus, mis muuhulgas koostab standardseid tabeleid ja graafikuid, ning paneb need ekraanil ruudustikule.</p>
                 <p>Iga graafik või tabel on jagatud sisemiselt kaheks: <em>display</em> ja <em>render</em> osa. <em>Display</em> on kindla kasutuse, ülesande ning andmeallikaga objekt. Näited on selle kuu parimate tabel või spetsiifiliselt bestika kogupunktide numbri element. Iga <em>display</em> küsib ise andmed ning laseb sobival <em>render</em> objektil teha saadud andmete põhjal graafik, tabel või muu näidik. <em>Render</em> objektid ongi näidikud, tabelid või graafikud, nagu näiteks joongraafik või punkte näitav element.</p>
                 <p>Andmed küsib <em>display</em> element sobiva GET päringuga, millele vastuseks saab genereeritud JSON formaadis andmed.</p>
-
-                <h2>Modernsemad ajad toovad paremad lahendused:</h2>
-                <p>Nüüdseks on punktisüsteem ja liikmehaldus viidud ühisesse andmebaasi, mistõttu on liikmete punktide pärimine selle võrra ka lihtsam.</p>
-            </div>
             </div>
         </div>
-            
+
     <?php
         require("../templates/footer.htm");
     ?>
